99af1fe5fc04707c6ede6c46c872c5558996eec6
Dmitriy <udv8@ya.ru>
2023-05-22 14:40:46 +0300

    v.0.3.2

--- dwg2txt.py
+++ dwg2txt.py
@@ -3,33 +3,17 @@
 """
 
 RAW_FILE = 'Новый текстовый документ.txt'
-NEW_FILE = 'new file_test.txt'
+NEW_FILE = 'new file.txt'
 START = 100
 
 
 def read_file(file_path):
-    return list([line for line in open(file_path)])
+    return list([line for line in open(file_path, encoding="utf-8")])
 
 
 def write_file(new_path_file, replaces_list):
     with open(new_path_file, 'w') as f:
         f.writelines(replaces_list)
-
-
-# def replace_file(file_path, new_path_file, start_point):
-#     with open(file_path, 'r') as file:
-#         i = start_point - 1
-#         with open(new_path_file, 'w') as f:
-#             for line in file:
-#                 if line.startswith(' '):
-#                     line = line.replace('          в точке  X=', ' ')
-#                     line = line.replace('  Y=', ' ')
-#                     new_line = line.split()
-#                     i += 1
-#                     s = '{} {} {}'.format(i, new_line[0], new_line[1])
-#                     s = s.replace(' ', '	')
-#
-#                     f.write(s + '\n')
 
 
 def replace_data(data_list, start_point):
